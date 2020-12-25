6dde66c7ce871d3d9c3ec92ea0749f24ca769b8d
guilhermemarx14 <guilhermemarx14@gmail.com>
2020-12-25 04:47:46 -0300

    AppBar background color

--- lib/util/constants.dart
+++ lib/util/constants.dart
@@ -3,4 +3,4 @@ import 'package:flutter/material.dart';
 Color appBarText = const Color.fromRGBO(110, 110, 110, 1);
 Color background = const Color.fromRGBO(40, 40, 40, 1);
 Color ytsGreen = const Color.fromRGBO(50, 168, 82, 1);
-Color appBarBackground = const Color.fromRGBO(23, 20, 16, 1);
+Color appBarBackground = const Color.fromRGBO(16, 16, 16, 1);
